bcf288f517cddfa7c6e73bdfb5d9cf8a46d5b7c0
Germán <coccagerman@gmail.com>
2021-06-22 20:47:14 -0300

    .

--- src/Components/NewNote/NewNote.jsx
+++ src/Components/NewNote/NewNote.jsx
@@ -18,9 +18,9 @@ export default function NewNote () {
             <p>Write a new note!</p>
             <article className='newNote'>
                 <input type="text" placeholder='Note tittle' onChange={e => setNoteTittle(e.target.value)} />
-                <textarea rows = "5" cols = "60" placeholder='Note content' onChange={e => setNoteContent(e.target.value)} />
-                <button className='btn-primary' onClick={() => onCreate()} >Submit</button>
+                <textarea rows = "5" placeholder='Note content' onChange={e => setNoteContent(e.target.value)} />
             </article>
+            <button className='btn-primary' onClick={() => onCreate()} >Submit</button>
         </div>
     )
 }
--- src/Components/SavedNote/SavedNote.jsx
+++ src/Components/SavedNote/SavedNote.jsx
@@ -1,25 +1,45 @@
+import { useState } from 'react'
+import { getFirestore } from '../../Services/Firebase'
 import { Icon } from '@iconify/react'
 import delete20Filled from '@iconify-icons/fluent/delete-20-filled'
 import bxsEdit from '@iconify-icons/bx/bxs-edit'
-import { getFirestore } from '../../Services/Firebase'
 
 export default function SavedNote ({tittle, content, id}) {
+
+    const [editingMode, setEditingMode] = useState(false)
+    const [editedTittle, setEditedTittle] = useState(tittle)
+    const [editedContent, setEditedContent] = useState(content)
+
+    const onEdit = () => {
+        const db = getFirestore()
+        const note = db.collection('notes').doc(id)
+    
+        note.set({noteTittle: editedTittle, noteContent: editedContent})
+        setEditingMode(false)
+    }
 
     const onDelete = () => {
         const db = getFirestore()
         const note = db.collection('notes').doc(id)
     
         note.delete()
-      }
+    }
 
     return (
         <div className='savedNote-container'>
             <Icon icon={delete20Filled} className='icon delete' onClick={() => onDelete()} />
-            <Icon icon={bxsEdit} className='icon edit' />
+            <Icon icon={bxsEdit} className='icon edit' onClick={() => setEditingMode(!editingMode)} />
             <article className='savedNote'>
-                <h2>{tittle}</h2>
-                <p>{content}</p>
+                {!editingMode ? <h2>{tittle}</h2> : <input type="text" placeholder={tittle} onChange={e => setEditedTittle(e.target.value)}/>}
+                {!editingMode ? <p>{content}</p> : <textarea rows = "5" placeholder={content} onChange={e => setEditedContent(e.target.value)} />}
             </article>
+
+            {editingMode ?
+                <div className='btn-container'>
+                    <button className='btn-secondary' onClick={() => setEditingMode(false)}>Cancel</button>
+                    <button className='btn-primary' onClick={() => onEdit()}>Save</button>
+                </div>
+            : null}
         </div>
     )
 }
