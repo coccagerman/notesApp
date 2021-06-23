f821720a3081dffc4aa6e49c24cdbed20fbbd05d
Germán <coccagerman@gmail.com>
2021-06-22 22:03:47 -0300

    Update NewNote.jsx

--- src/Components/NewNote/NewNote.jsx
+++ src/Components/NewNote/NewNote.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { getFirestore } from '../../Services/Firebase'
 
 export default function NewNote () {
@@ -6,21 +6,27 @@ export default function NewNote () {
     const [noteTittle, setNoteTittle] = useState('')
     const [noteContent, setNoteContent] = useState('')
 
-    const onCreate = () => {
+    const titleRef = useRef()
+    const contentRef = useRef()
+
+    const onSubmit = () => {
         const db = getFirestore()
         const notes = db.collection('notes')
     
         notes.add({noteTittle: noteTittle, noteContent: noteContent})
+        titleRef.current.value = ''
+        contentRef.current.value = ''
     }
 
     return (
         <div className='newNote-container'>
             <p>Write a new note!</p>
             <article className='newNote'>
-                <input type="text" placeholder='Note tittle' onChange={e => setNoteTittle(e.target.value)} />
-                <textarea rows = "5" placeholder='Note content' onChange={e => setNoteContent(e.target.value)} />
+                <input type="text" placeholder='Note tittle' ref={titleRef} onChange={e => setNoteTittle(e.target.value)} />
+                <textarea rows = "5" placeholder='Note content' ref={contentRef} onChange={e => setNoteContent(e.target.value)} />
             </article>
-            <button className='btn-primary' onClick={() => onCreate()} >Submit</button>
+
+            <button className='btn-primary' onClick={() => onSubmit()} >Submit</button>
         </div>
     )
 }
